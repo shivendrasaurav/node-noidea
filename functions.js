da4b68f0568ef2820c153651b0f3b508497951e5
shubhambaid <shubhambaid99@gmail.com>
2020-04-26 16:12:10 +0530

    variable tag change in form

--- functions.js
+++ functions.js
@@ -1,19 +1,18 @@
 const userId = document.getElementById('userId');
-const firstName = document.getElementById('firstName');
-const lastName = document.getElementById('lastName');
-const age = document.getElementById('age');
+const firstName = document.getElementById('phonenumber');
+const lastName = document.getElementById('address');
+const age = document.getElementById('pincode');
 const addBtn = document.getElementById('addBtn');
-const updateBtn = document.getElementById('updateBtn');
-const removeBtn = document.getElementById('removeBtn'); 
+
 
 const database = firebase.database();
 const usersRef = database.ref('/users');
 addBtn.addEventListener('click', e => {
   e.preventDefault();
   usersRef.child(userId.value).set({
-    first_name: firstName.value,
-    last_name: lastName.value,
-    age: age.value
+    contact_number: phonenumber.value,
+    pickup_address: address.value,
+    area_pincode: pincode.value
   });
 });
 
